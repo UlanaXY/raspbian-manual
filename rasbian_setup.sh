3905a31ac9814a2fafc23886faf97116c5741d67
Mikolaj <mikolaj.marczak@protonmail.com>
2025-12-09 14:46:23 +0100

    legacy version

--- rasbian_setup.sh
+++ rasbian_setup.sh
@@ -3,6 +3,3 @@ echo "Starting rasbian_setup.sh"
 sudo apt update
 sudo apt -y full-upgrade
 sudo apt -y install powerline git neovim fzf ripgrep fd-find
-
-# todo lazyvim and lazygit setup
-# git clone https://github.com/UlanaXY/uln-lazyvim-starter.git ~/.config/nvim
